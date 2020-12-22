b8b28ecfd46fed04f0f1ca8799f02fd6c9c5e794
Aaron Shafovaloff <aaronshaf@gmail.com>
2020-12-22 17:07:17 -0500

    Updates

---
+++ precache-manifest.d606f7d3b898655bcaa4d940ee14bf6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9faed81fb6192cab4c950c2d9987fdb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "23b0eab8a1690516bca4",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "81cf0301376471f761a1",
+    "url": "./static/js/2.5945ebdf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5945ebdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23b0eab8a1690516bca4",
+    "url": "./static/js/main.dfeefbf1.chunk.js"
+  },
+  {
+    "revision": "946c7ae712c5dd6a5eba",
+    "url": "./static/js/runtime-main.4d8d1fe4.js"
+  }
+]);
